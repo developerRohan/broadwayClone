275bc689640ec4082716f5737aff32228fcd0abf
MY RAILS <jhambrohan44@gmail.com>
2017-05-03 02:27:40 +0530

    added image corrections

--- app/models/play.rb
+++ app/models/play.rb
@@ -2,8 +2,8 @@ class Play < ApplicationRecord
 belongs_to :user
 belongs_to :category
 validates :title , presence: true , length: {minimum: 3 , maximum: 50} 
-validates :description , presence: true , length: {minimum: 50 , maximum: 500}
+validates :description , presence: true , length: {minimum: 50 , maximum: 5000}
 validates :director , presence: true , length: {minimum: 5 , maximum: 50} 
-has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
+has_attached_file :play_img, :styles => { :play_index => "500x500>", :play_show => "100x100>" }, default_url: "/images/:style/missing.png"
+validates_attachment_content_type :play_img, :content_type => /\Aimage\/.*\z/
 end

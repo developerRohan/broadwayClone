950b422b8e9e8fa45562f0caef7e46a78d92028a
MY RAILS <jhambrohan44@gmail.com>
2017-05-03 01:42:27 +0530

    added paperclip gem

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise', '~> 4.2', '>= 4.2.1'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'paperclip', '~> 5.1'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/models/play.rb
+++ app/models/play.rb
@@ -4,4 +4,6 @@ belongs_to :category
 validates :title , presence: true , length: {minimum: 3 , maximum: 50} 
 validates :description , presence: true , length: {minimum: 50 , maximum: 500}
 validates :director , presence: true , length: {minimum: 5 , maximum: 50} 
+has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end

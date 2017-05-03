18dde270d82321b40a4f37e626000ab1aa186809
MY RAILS <jhambrohan44@gmail.com>
2017-05-03 10:14:19 +0530

    added routes for reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :plays
-  has_many :reviews
+  has_many :review
 
 end
